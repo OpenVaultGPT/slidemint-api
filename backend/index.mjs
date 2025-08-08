08ffd1e4126cd621f0f609e4e49ecc0801b890db
grantjones1 <54856253+grantjones1@users.noreply.github.com>
2025-08-08 14:23:07 +0100

    Update index.mjs

--- backend/index.mjs
+++ backend/index.mjs
@@ -13,8 +13,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
-import cors from 'cors';
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
